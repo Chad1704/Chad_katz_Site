8f78ecdea3493c31794544626973cd355806bc72
Chad1704 <chadjoshkatz@gmail.com>
2025-05-12 17:03:09 -0400

    testing div effect

--- chad_site/src/app/contact.jsx
+++ chad_site/src/app/contact.jsx
@@ -22,21 +22,22 @@ function Contact() {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex flex-col items-center gap-4">
+    <div className="flex justify-center items-center h-screen ">
+      <div className="flex flex-col items-center gap-4 font-mono ">
         {menuItems.map((item, index) => (
           <a
             key={item.text}
             href={`#${item.text.toLowerCase()}`} // Change the href to the link you want
             target="_self" // Adjust as needed (e.g., _blank for new tab)
-            className="btn"
+            className="btn_2"
             onMouseEnter={() => handleMouseEnter(index)}
             onMouseLeave={() => handleMouseLeave(index)}
           >
-            <div className="flex items-center gap-2">
+            <div className="flex w-full h-full items-center justify-center ">
               {/* Render the icon and text */}
-              <span>{item.icon}</span>
-              <TextEffect text={item.text} trigger={triggers[index]} />
+              <span className='flex justify-center bg-green-500'>{item.icon}</span>
+              <div className='flex items-center justify-center w-full h-full bg-amber-400'>
+              <TextEffect className="" text={item.text} trigger={triggers[index]} /></div>
             </div>
           </a>
         ))}
